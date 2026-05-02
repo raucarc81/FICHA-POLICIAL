5d8a8bc11b86bdee7c15982c0dbd3ad82b23c62f
raucarc81 <rauca.rc.81@gmail.com>
2026-05-02 12:13:09 +0200

    Update service worker cache version to v11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cal-policial-v10';
+const CACHE='cal-policial-v11';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
 
 self.addEventListener('install',e=>{
@@ -7,16 +7,10 @@ self.addEventListener('install',e=>{
 });
 
 self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>
-      Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
 
 self.addEventListener('fetch',e=>{
-  e.respondWith(
-    caches.match(e.request).then(cached=>cached||fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request)));
 });
